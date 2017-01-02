42cea2900baf355acbeca21193a634bceec6c437
Carlos Nunez <dev@carlosnunez.me>
2017-01-02 17:28:47 +0000

    file rename to fix typo

--- gloabal_variables.tf
+++
@@ -1,8 +0,0 @@
-variable "global_infrastructure_tags" {
-  description = "Tags to apply onto all infrastructure created by this deployment."
-  default {}
-}
-
-variable "environment" {
-  description = "Environment this infrastrucutre is targetting."
-}
