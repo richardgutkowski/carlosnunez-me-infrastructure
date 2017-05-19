c4ba3d70c80e2b8ba761199946539308449c16c0
Carlos Nunez <dev@carlosnunez.me>
2017-05-19 10:20:59 -0400

    cnunez | Removed all of what I had before.

--- config.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "tfstate" {
-  backend = "s3"
-  config {
-    bucket = "${var.terraform_remote_state_bucket_name}"
-    region = "${var.terraform_remote_state_region_name}"
-    key = "${var.terraform_remote_state_key_name}"
-  }
-}
--- variables_route53.tf
+++
@@ -1,3 +0,0 @@
-variable "route53_hosted_zone_id" {
-  description = "Default Route53 hosted zone ID to use."
-}
