fe195c78cba2d69f897d82427224fda702c1f6a6
Carlos Nunez <dev@carlosnunez.me>
2017-05-20 08:53:34 -0500

    cnunez | moved config out of spec and into lib

--- spec/config.rb
+++
@@ -1,6 +0,0 @@
-def load_config
-  if not File.exist? 'config/infrastructure_config.yml'
-    raise FileNotFoundException, 'config/infrastructure_config.yml not found' 
-  end
-  YAML.load_file("config/infrastructure_config.yml")
-end
