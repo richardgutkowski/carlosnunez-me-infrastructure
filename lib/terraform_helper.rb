bb455a7d0d16909e02c12e398dd17994ac7e34b8
Carlos Nunez <dev@carlosnunez.me>
2017-05-20 09:49:55 -0500

    Auto-commit: saved lib/terraform_helper.rb, cnunez| make it more readable by moving nonexported methods underneath private

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -1,6 +1,22 @@
 require 'net/http'
 require 'open-uri'
 
+def install_latest_version_of_terraform
+  os = get_supported_terraform_os_build
+  if os == "unsupported"
+    raise "Terraform is not supported by your operating system."
+  end
+
+  cpu_platform = get_supported_cpu_platform
+  if cpu_platform == "unsupported"
+    raise "Terraform is not supported by your CPU platform."
+  end
+
+ 
+  terraform_binary = open("https://releases.hashicorp.com/terraform/#{latest_version}/#{terraform_file_to_get}")
+end
+
+private
 def get_supported_terraform_os_build
   case RUBY_PLATFORM
   when /win32/
@@ -29,17 +45,7 @@ def get_supported_cpu_platform
   end
 end
 
-def install_latest_version_of_terraform
-  os = get_supported_terraform_os_build
-  if os == "unsupported"
-    raise "Terraform is not supported by your operating system."
-  end
-
-  cpu_platform = get_supported_cpu_platform
-  if cpu_platform == "unsupported"
-    raise "Terraform is not supported by your CPU platform."
-  end
-
+def get_latest_release_of_terraform
   terraform_releases_uri = URI('https://releases.hashicorp.com')
   terraform_releases_html = Net::HTTP.get(terraform_releases_uri).split("\n")
   terraform_versions = terraform_releases_html.map do |html_node|
@@ -47,7 +53,6 @@ def install_latest_version_of_terraform
     version
   end.compact
   _, latest_version = terraform_versions.first.split('_')
- 
-   
-  terraform_binary = open("https://releases.hashicorp.com/terraform/#{latest_version}/#{terraform_file_to_get}")
+
+  terraform_file_to_get
 end
