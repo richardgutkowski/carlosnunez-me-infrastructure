f37dea4df8ec275aaa25c8d93fb33fe034b4b4ca
Carlos Nunez <dev@carlosnunez.me>
2017-05-20 09:36:46 -0500

    Auto-commit: saved lib/terraform_helper.rb, cnunez| Extract Terraform versions from html page.

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -2,5 +2,9 @@ require 'net/http'
 
 def install_latest_version_of_terraform
   terraform_releases_uri = URI('https://releases.hashicorp.com')
-  terraform_releases_html = Net::HTTP.get(terraform_releases_uri)
+  terraform_releases_html = Net::HTTP.get(terraform_releases_uri).split("\n")
+  terraform_versions = terraform_releases_html.map do |html_node|
+    version = html_node.gsub!  /.*>(terraform_.*)<.*/,'\1'
+    version
+  end.compact
 end
