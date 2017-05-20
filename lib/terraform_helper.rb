d96b9b8c737e264707d8c97a6a50db5650ee8144
Carlos Nunez <dev@carlosnunez.me>
2017-05-20 09:42:21 -0500

    Auto-commit: saved lib/terraform_helper.rb, cnunez| Check whether Terraform is supported by this OS before doing any downloading.

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -18,6 +18,10 @@ def get_supported_terraform_os_build
 end
 
 def install_latest_version_of_terraform
+  os = get_supported_terraform_os_build
+  if os == "unsupported"
+    raise "Terraform is not supported by your OS [#{RUBY_PLATFORM}]."
+  end
   terraform_releases_uri = URI('https://releases.hashicorp.com')
   terraform_releases_html = Net::HTTP.get(terraform_releases_uri).split("\n")
   terraform_versions = terraform_releases_html.map do |html_node|
