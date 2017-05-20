6e4e0228355dfbf4c0db06d4f353d5cf959f801c
Carlos Nunez <dev@carlosnunez.me>
2017-05-20 09:35:17 -0500

    Auto-commit: saved lib/terraform_helper.rb, cnunez| Actually, never mind about the proxy thing.

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -2,8 +2,5 @@ require 'net/http'
 
 def install_latest_version_of_terraform
   terraform_releases_uri = URI('https://releases.hashicorp.com')
-  if ENV['TERRAFORM_RELEASES_URI']
-    terraform_releases_uri = URI(ENV['TERRAFORM_RELEASES_URI'])
-  end
-  terraform_releases_html = Net::HTTP.get(URI('https://releases.hashicorp.com'))
+  terraform_releases_html = Net::HTTP.get(terraform_releases_uri)
 end
