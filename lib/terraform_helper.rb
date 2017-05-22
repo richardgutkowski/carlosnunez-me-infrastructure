91260a5c06e57dd9eefaebf2f7aa1623e1ee7b2b
Carlos Nunez <dev@carlosnunez.me>
2017-05-22 07:30:20 -0500

    Auto-commit: saved lib/terraform_helper.rb, cnunez| I forgot that the link to Terraform releases is forwarded. Wrote a method to account for this. (I am not using curl because we can't depend on it being available on every operating system whereas Net::HTTP will be.

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -48,17 +48,34 @@ end
 
 def get_latest_terraform_release(os: ,cpu_platform:)
   terraform_releases_uri = 'https://releases.hashicorp.com/terraform'
+  terraform_releases_html = do_http_get_with_forwards! uri:terraform_releases_uri
   terraform_releases_html = Net::HTTP.get(URI(terraform_releases_uri)).split("\n")
   terraform_versions = terraform_releases_html.map do |html_node|
     version = html_node.gsub!  /.*>(terraform_.*)<.*/,'\1'
     version
   end.compact
-  require 'pry'
-  binding.pry
   _, latest_version = terraform_versions.first.split('_')
   latest_terraform_release_uri = \
     "#{terraform_releases_uri}/#{latest_version}/terraform_#{latest_version}_#{os}_#{cpu_platform}.zip"
   latest_terraform_release_uri
+end
+
+def do_http_get_with_forwards!(uri:, redirect_limit = 10)
+  loop do
+    break if redirect_limit == 0
+    uri_with_data = URI(uri)
+    request = Net::HTTP.get(uri_with_data)
+    response = Net::HTTP.start(uri_with_data.host, uri_with_data.port) do |session|
+      session.request(request)
+    end
+    case response
+    when Net::HTTPSuccess then response
+    when Net::HTTPRedirection then
+      do_http_get_with_forwards! uri: uri, redirect_limit: redirect_limit-1
+    else
+      response.error!
+    end
+  end
 end
 
 def download_terraform_to_working_directory!(uri_as_string:)
