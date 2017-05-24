0d71f95c9a05f15b0da02729e6712946b20cdb7e
Carlos Nunez <dev@carlosnunez.me>
2017-05-24 18:01:07 -0400

    Auto-commit: saved lib/terraform_helper.rb, cnunez| debug version check

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -16,6 +16,8 @@ def install_latest_version_of_terraform!
 
   latest_terraform_release_details = get_latest_terraform_release os:os, cpu_platform:cpu_platform
   latest_terraform_version = latest_terraform_release_details[:latest_version]
+  require 'pry'
+  binding.pry
   latest_terraform_release_uri = latest_terraform_release_details[:latest_version_uri]
   if latest_terraform_release_uri == "NOT_FOUND"
     raise "Couldn't retrieve latest the link to the latest version of Terraform. You'll need to install it manually."
