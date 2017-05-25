123cf06978e7a71856f9bd6d36c2c94ecd325510
Carlos Nunez <dev@carlosnunez.me>
2017-05-25 11:00:09 -0400

    Auto-commit: saved lib/config.rb, cnunez| Allow support for YML and YAML.

--- lib/config.rb
+++ lib/config.rb
@@ -1,7 +1,15 @@
 def load_config(environment:)
-  file_to_load = "config/infrastructure_config.#{environment}.yml"
-  if not File.exist? file_to_load
-    raise "#{file_to_load} not found."
+  name_of_file_to_load = "config/infrastructure_config.#{environment}"
+  valid_yaml_extensions = ['yml','yaml']
+  valid_config_files_found = valid_yaml_extensions.map do |extension|
+    file_to_find = "#{name_of_file_to_load}.#{extension}"
+    if File.exist? file_to_find
+      file_to_find
+    end
+  end.compact
+  if valid_config_files_found.count > 1
+    raise "Too many files found: #{valid_config_files_found}"
   end
+  file_to_load = valid_config_files_found.first
   YAML.load_file(file_to_load)
 end
