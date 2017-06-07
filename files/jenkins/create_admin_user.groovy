0683991bb56f1ce177bdfff64b48b002f327cebe
Carlos Nunez <dev@carlosnunez.me>
2017-06-07 16:37:54 -0500

    [/home/cnunez/src/infrastructure/files/jenkins/create_admin_user.groovy] cnunez | I forgot that my Groovy script was resetting the authorization strategy. I've met the enemy and the enemy is MY OWN IGNORANCE

--- files/jenkins/create_admin_user.groovy
+++ files/jenkins/create_admin_user.groovy
@@ -10,8 +10,3 @@ println "--> creating local user 'admin'"
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount('admin','password')
 instance.setSecurityRealm(hudsonRealm)
-
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-instance.setAuthorizationStrategy(strategy)
-instance.save()
