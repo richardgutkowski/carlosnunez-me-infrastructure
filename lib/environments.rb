dd33f354ef8cb74bc985c4ab5d64652da2771489
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 18:52:53 -0500

    [lib/environments.rb] cnunez | Start using S3 to capture valid envs

--- lib/environments.rb
+++ lib/environments.rb
@@ -1,4 +1,7 @@
 def get_supported_environments
+  require 'aws-sdk'
+  s3_client = Aws::S3::Client.new
+  our_buckets = s3_client.list_buckets
   Dir.glob('config/*.yaml').map do |file|
     file.split('.')[1]
   end
