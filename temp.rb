9864d94b206fd3d234a6ee807000256b034d339a
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:50:41 -0500

    [temp.rb] cnunez | Yep, that didn't work.

--- temp.rb
+++ temp.rb
@@ -1,3 +1,3 @@
-#!/usr/bin/env ruby
-system('terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null')
-exec('terraform plan -state=nil -out=terraform2.tfplan > /dev/null')
+terraform_plan_json_str = system('./terraform plan -state=no_state_for_testing \
+                                      -out=terraform.tfplan > /dev/null ; \
+                                      [ -f terraform.tfplan ] && tfjson terraform.tfplan')
