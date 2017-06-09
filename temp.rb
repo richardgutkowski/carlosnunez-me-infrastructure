0186992f940cffde1e83e950c15de8e3107beb39
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:41:01 -0500

    [temp.rb] cnunez | Let's see what happens when I use `exec` instead of `system`

--- temp.rb
+++ temp.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-_ = system('terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null')
+system('terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null')
+exec('terraform plan -state=nil -out=terraform2.tfplan > /dev/null')
