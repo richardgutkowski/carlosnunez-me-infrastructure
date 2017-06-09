48657bb828ecd19bf1737ad991b5d97b49d66fae
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 15:51:41 -0500

    [temp.rb] cnunez | \%x didn't work. let's try system.

--- temp.rb
+++ temp.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'rake'
-%x{'[ -f "terraform.tfplan" ] && rm terraform.tfplan'}
-%x{'terraform plan -state=nil -out=terraform.tfplan > /dev/null'}
+system('[ -f "terraform.tfplan" ] && rm terraform.tfplan')
+system('terraform plan -state=nil -out=terraform.tfplan > /dev/null')
