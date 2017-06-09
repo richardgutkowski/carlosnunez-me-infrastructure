9a8fc4cfa745495b35201e77c955f156a3356660
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 19:43:56 -0500

    [spec/infrastructure//core/vpc_spec.rb] cnunez | Just show me if the terraform plan ran"

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -1,19 +1,3 @@
 require 'spec_helper'
 
-vpcs = @infrastructure_config['aws']['vpc']
-vpcs.each do |vpc|
-end
-
-# --- old ---
-vpcs = @infrastructure_config['vpc']
-vpcs.each do |vpc|
-  vpc_name = vpc['name']
-  vpc_cidr_block = vpc['cidr_block']
-  vpc_tags = vpc['tags']
-  describe vpc(vpc_name) do
-    it { should exist }
-    it { should be_available }
-    its('is_default') { should be_false }
-    its('cidr_block') { should eq vpc_cidr_block }
-  end
-end
+puts "Terraform plan generated: #{$terraform_plan}"
