687a0365f3c7e62affd576c612e5fb7ed429752c
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:24:28 -0500

    [temp.rb] cnunez | This has nothing to do with Rake. This command works fine in irb but bombs when run under ruby outright.

--- temp.rb
+++ temp.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-  `./terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null;  \
-      [ -f terraform.tfplan ] && tfjson terraform.tfplan`
+puts `./terraform plan -state=no_state_for_testing -out=terraform.tfplan`
