2341134358f67ba6368db43b5ff5e954824bbe28
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 20:10:11 -0500

    [spec/infrastructure/core/vpc_spec.rb] cnunez | Better englihs

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'awspec'
 
 describe "Terraform plan" do
-  it "Should exist" do
+  it "should exist" do
     puts "Terraform plan: #{$terraform_plan}, is_nil: #{$terraform_plan.nil?}"
     $terraform_plan.should_not be_nil
   end
