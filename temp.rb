fb544adda6bfdae5ceb677666d20f99063cd9864
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:17:45 -0500

    [temp.rb] cnunez | This got even weirder\! Open3 has nothing to do with it\! Maybe it's something within Rake?

---
+++ temp.rb
@@ -0,0 +1,4 @@
+terraform_plan_json_str = \
+  `./terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null;  \
+      [ -f terraform.tfplan ] && tfjson terraform.tfplan`
+puts terraform_plan_json_str
