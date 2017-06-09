13ca66ac5e4e7e26ca4a0a999385209935cb9ecf
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 20:14:26 -0500

    [spec/infrastructure/core/vpc_spec.rb] cnunez | Properly test that stderr is nil

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 require 'awspec'
 
 describe "Terraform plan" do
+  it "should not have failed to run" do
+    puts "Terraform plan errors: #{$terraform_plan_stdout}"
+    $terraform_plan_stdout.should be_nil
+  end
   it "should exist" do
-    puts "Terraform plan: #{$terraform_plan}, is_nil: #{$terraform_plan.nil?}"
     $terraform_plan.should_not be_nil
   end
 end
