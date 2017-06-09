fbd65a2b5ea823f163dc35039d9a6006aebe83c6
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:07:46 -0500

    [temp.rb] cnunez | What happens if I change the file name of the dummy state file to 'nil'?

--- temp.rb
+++ temp.rb
@@ -1,4 +1,4 @@
 require 'rake'
-system('./terraform plan -state=dummy_state -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
+system('./terraform plan -state=nil -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
 terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
-system('rm -rf ./dummy_state ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
+system('rm -rf ./nil ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
