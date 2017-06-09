1e0b0ce6b748bab2544daae9fcd7092425e2c30b
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:11:07 -0500

    [temp.rb] cnunez | What happens when I call the plan file 'terraform.tfplan' instead of something custom?

--- temp.rb
+++ temp.rb
@@ -6,6 +6,6 @@ system('rm terraform.tfplan')
 
 # ---
 puts 'attempt 2'
-system('./terraform plan -state=dummy_state -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
-terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
-system('rm -rf ./dummy_state ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
+system('./terraform plan -state=dummy_state -out=terraform.tfplan > /dev/null')
+terraform_plan_as_json_str = system('tfjson ./terraform.tfplan')
+system('rm -rf ./dummy_state terraform.tfplan')
