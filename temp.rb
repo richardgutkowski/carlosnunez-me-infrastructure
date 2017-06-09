1fff4e5788f8da05cc9e5717218c284759786e4d
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 15:57:11 -0500

    [temp.rb] cnunez | Yep, it was the spaces. But I don't know why.

--- temp.rb
+++ temp.rb
@@ -3,10 +3,3 @@ require 'rake'
 system('terraform plan -state=nil -out=terraform.tfplan > /dev/null')
 system('tfjson terraform.tfplan')
 system('rm terraform.tfplan')
-
-#---
-
-system('./terraform plan -state=nil \
-												 -out=terraform2.tfplan ; \
-				tfjson terraform2.tfplan ; \
-				rm terraform2.tfplan')
