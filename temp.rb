aa0b0665b463502df26dda2887c680177590480b
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:52:16 -0500

    [temp.rb] cnunez | Yep, that didn't work.

--- temp.rb
+++ temp.rb
@@ -1,3 +1,2 @@
 _ = system('./terraform plan -state=no_state_for_testing \
                                       -out=terraform.tfplan > /dev/null')
-terraform_plan_json_str = system('tfjson ./terraform.tfplan')
