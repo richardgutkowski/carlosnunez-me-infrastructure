adcee7d0db7f2951dbd38940b617017e15100efe
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 15:47:36 -0500

    [temp.rb] cnunez | rm -q

--- temp.rb
+++ temp.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'rake'
-sh %{'[ -f terraform.tfplan ] && rm terraform.tfplan'}
+sh %{'rm -q terraform.tfplan'}
 sh %{'terraform plan -state=nil -out=terraform.tfplan > /dev/null'}
