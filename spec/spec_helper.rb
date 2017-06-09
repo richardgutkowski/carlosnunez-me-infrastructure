dada7bf5831d688aa3f1761a2329848ba57abdf7
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:05:08 -0500

    [spec/spec_helper.rb] cnunez | _as_

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ RSpec.configure do |config|
 		system('./terraform plan -state=dummy_state -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
 		terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
 		system('rm -rf ./dummy_state ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
-    if terraform_plan_json_str == ""
+    if terraform_plan_as_json_str == ""
       raise "Mock Terraform plan was not generated."
     end
     $terraform_plan = JSON.parse(terraform_plan_json_str)
