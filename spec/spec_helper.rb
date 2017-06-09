0d4c1233ea98a1970ed1e24c59ddb4eb2ebd65e2
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:02:22 -0500

    [spec/spec_helper.rb] cnunez | Let's try this now\!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,9 @@ end
 
 RSpec.configure do |config|
   config.before(:all) {
-    terraform_plan_json_str = system('./terraform plan -state=no_state_for_testing \
-                                     -out=terraform.tfplan > /dev/null ; \
-                                     [ -f terraform.tfplan ] && tfjson terraform.tfplan')
+		system('./terraform plan -state=dummy_state -out=.fixtures/${TARGET_ENVIRONMENT}_infra.tfplan > /dev/null')
+		terraform_plan_as_json_str = system('tfplan ./.fixtures/${TARGET_ENVIRONMENT}_infra.tfplan')
+		system('rm -rf ./dummy_state .fixtures/${TARGET_ENVIRONMENT}_infra.tfplan')
     if terraform_plan_json_str == ""
       raise "Mock Terraform plan was not generated."
     end
