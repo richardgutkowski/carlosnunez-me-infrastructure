41b8e64892be4793731ded1a816c94f3abf11c23
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 19:18:03 -0500

    [lib/environments.rb] cnunez | Use the state bucket variable insetad of the base infrastructure bucket one. See the previous commit for why.

--- lib/environments.rb
+++ lib/environments.rb
@@ -1,9 +1,9 @@
 def get_supported_environments
   require 'aws-sdk'
-  return nil if !ENV['AWS_S3_INFRASTRUCTURE_BUCKET']
+  return nil if !ENV['AWS_S3_TERRAFORM_STATE_BUCKET']
+  bucket_to_query = ENV['AWS_S3_TERRAFORM_STATE_BUCKET'].split('/')[0]
   s3_client = Aws::S3::Client.new
-  objects_in_infrastructure_bucket =
-    s3_client.list_objects(bucket: ENV['AWS_S3_INFRASTRUCTURE_BUCKET'])
+  objects_in_infrastructure_bucket = s3_client.list_objects(bucket: bucket_to_query)
   tfvars_found = objects_in_infrastructure_bucket.select do |s3_object|
     s3_object.key.include? 'tfvars/'
   end.map(&:key)
