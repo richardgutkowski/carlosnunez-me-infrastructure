a19f3347df62899cc657d99261bc975abcb64255
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:08:22 -0500

    [temp.rb] cnunez | That didnt' work. Does the same encoding error occur when I restore the results back?

--- temp.rb
+++ temp.rb
@@ -1,4 +1,4 @@
 require 'rake'
-system('./terraform plan -state=nil -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
-terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
-system('rm -rf ./nil ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
+system('terraform plan -state=nil -out=terraform.tfplan > /dev/null')
+str = system('tfjson terraform.tfplan')
+system('rm terraform.tfplan')
