95a65fa9f8454499ba81287b2ef3ba4efb1ee918
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 14:10:31 -0500

    [spec/spec_helper.rb] cnunez | Okay, so I think that Open3.capture3 does something strange with the encoding of stdout that corrupts the resulting tfplan generated. I tried looking into gob to see what's up, but I didn't get too far. It also happens that I don't even need the output generated by terraform itself; I only need the output from tfplan. So I got rid of open3 and used good ol' backticks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'yaml'
 require 'rspec'
-require 'open3'
 
 if not ENV['TARGET_ENVIRONMENT']
   raise 'TARGET_ENVIRONMENT not found in your environment; please define it.'
@@ -8,16 +7,15 @@ end
 
 RSpec.configure do |config|
   config.before(:all) {
-    _, $terraform_plan_stderr, _ = 
-      Open3.capture3("./terraform plan -state=nil_state_for_testing -out=temp.tfplan")
-    puts "tfplan: #{File.read('./temp.tfplan')}"
-    terraform_plan_json_str = `[ -f temp.tfplan ] && tfjson temp.tfplan`
-    if not File.exist? 'temp.tfplan' or terraform_plan_json_str == ""
+    terraform_plan_json_str = \
+      `./terraform plan -state=no_state_for_testing -out=terraform.tfplan > /dev/null \
+      [ -f terraform.plan ] && tfjson terraform.plan`
+    if terraform_plan_json_str == ""
       raise "Mock Terraform plan was not generated."
     end
     $terraform_plan = JSON.parse(terraform_plan_json_str)
   }
   config.after(:all) {
-    _ = `rm temp.tfplan`
+    `rm terraform.tfplan`
   }
 end
