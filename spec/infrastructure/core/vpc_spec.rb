67987ef157108c5c25a53604d5fe269e08d218f4
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 20:21:37 -0500

    [spec/infrastructure/core/vpc_spec.rb] cnunez | `be_empty`, not `be_nil`

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -4,9 +4,9 @@ require 'awspec'
 describe "Terraform plan" do
   it "should not have failed to run" do
     puts "Terraform plan errors: #{$terraform_plan_stdout}"
-    $terraform_plan_stderr.should be_nil
+    $terraform_plan_stderr.should be_empty
   end
   it "should exist" do
-    $terraform_plan.should_not be_nil
+    $terraform_plan.should_not be_empty
   end
 end
