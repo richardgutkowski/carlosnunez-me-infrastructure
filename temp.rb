2f7843b499649f60f5f9bfbf518d35a73944ea38
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 15:49:22 -0500

    [temp.rb] cnunez | Let's try this with \%x.

--- temp.rb
+++ temp.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'rake'
-sh 'rm -q terraform.tfplan'
-sh %{'terraform plan -state=nil -out=terraform.tfplan > /dev/null'}
+%x('[ -f terraform.tfplan ] && rm terraform.tfplan')
+%x('terraform plan -state=nil -out=terraform.tfplan > /dev/null')
