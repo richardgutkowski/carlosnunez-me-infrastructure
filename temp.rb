c27ef5f086f568efa5c7a28f353060ad0fb98392
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:09:42 -0500

    [temp.rb] cnunez | Let's compare the two mechanisms that I have to create and parse a tfplan. What's the difference between them?

--- temp.rb
+++ temp.rb
@@ -5,4 +5,6 @@ system('rm terraform.tfplan')
 
 # ---
 
-
+system('./terraform plan -state=dummy_state -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
+terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
+system('rm -rf ./dummy_state ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
