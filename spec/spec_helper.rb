4a7b2ecffd4d49dd5cb5ee687bf56856d7c36205
Carlos Nunez <dev@carlosnunez.me>
2017-06-09 16:04:17 -0500

    [spec/spec_helper.rb] cnunez | just leave the fixture in the cwd

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,9 @@ end
 
 RSpec.configure do |config|
   config.before(:all) {
-		system('./terraform plan -state=dummy_state -out=.fixtures/${TARGET_ENVIRONMENT}_infra.tfplan > /dev/null')
-		terraform_plan_as_json_str = system('tfplan ./.fixtures/${TARGET_ENVIRONMENT}_infra.tfplan')
-		system('rm -rf ./dummy_state .fixtures/${TARGET_ENVIRONMENT}_infra.tfplan')
+		system('./terraform plan -state=dummy_state -out=${TARGET_ENVIRONMENT}_infra_fixture.tfplan > /dev/null')
+		terraform_plan_as_json_str = system('tfjson ./${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
+		system('rm -rf ./dummy_state ${TARGET_ENVIRONMENT}_infra_fixture.tfplan')
     if terraform_plan_json_str == ""
       raise "Mock Terraform plan was not generated."
     end
