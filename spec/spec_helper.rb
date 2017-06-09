3aefa196dafeeecf063bbc97f1fc033eff5f3469
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 20:13:40 -0500

    [spec/spec_helper.rb] cnunez | Capture the stdout too

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'rspec'
+require 'open3'
 
 if not ENV['TARGET_ENVIRONMENT']
   raise 'TARGET_ENVIRONMENT not found in your environment; please define it.'
@@ -7,6 +8,7 @@ end
 
 RSpec.configure do |config|
   config.before(:all) {
-    $terraform_plan = `terraform plan`
+    $terraform_plan, $terraform_plan_stdout, _ = 
+      Open3.capture("terraform plan -state=nil_state_for_testing")
   }
 end
