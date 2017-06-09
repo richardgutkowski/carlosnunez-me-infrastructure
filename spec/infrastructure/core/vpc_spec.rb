4be6bbea668865221bd0225584f8ff12d172576e
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 21:26:47 -0500

    [spec/infrastructure/core/vpc_spec.rb] cnunez | should be empty

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -3,7 +3,6 @@ require 'awspec'
 
 describe "Terraform plan" do
   it "should not have failed to run" do
-    puts "Terraform plan errors: #{$terraform_plan_stdout}"
     $terraform_plan_stderr.should be_empty
   end
   it "should exist" do
