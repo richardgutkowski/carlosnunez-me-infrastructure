2306001f17bee1c52dae7478a300779639fc703a
Carlos Nunez <dev@carlosnunez.me>
2017-06-08 19:02:13 -0500

    [lib/environments.rb] cnunez | Finished implementation.

--- lib/environments.rb
+++ lib/environments.rb
@@ -1,8 +1,14 @@
 def get_supported_environments
   require 'aws-sdk'
+  return [] if !ENV['AWS_S3_INFRASTRUCTURE_BUCKET']
   s3_client = Aws::S3::Client.new
-  our_buckets = s3_client.list_buckets
-  Dir.glob('config/*.yaml').map do |file|
-    file.split('.')[1]
+  objects_in_infrastructure_bucket =
+    s3_client.list_objects(bucket: ENV['AWS_S3_INFRASTRUCTURE_BUCKET'])
+  tfvars_found = objects_in_infrastructure_bucket.select do |s3_object|
+    s3_object.key.include? 'tfvars/'
+  end.map(&:key)
+  environments_found = tfvars_found.map do |s3_object_key|
+    s3_object_key.split('/')[1]
   end
+  environments_found
 end
