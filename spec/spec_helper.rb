1fa66ee438c97daa56a8736e2e87c728898d48c6
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 22:35:06 -0500

    [spec/spec_helper.rb] cnunez | Kernel.` for terraform plan isf ine.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,14 +9,16 @@ RSpec.configure do |config|
   config.before(:all) {
     tfjson_location = [ ENV['GOPATH'], 'bin', 'tfjson' ].join('/')
     if not File.exist? tfjson_location
-      raise "tfjson not found at #{tfjson_location}. Rakefile should have installed it..."
+      raise "tfjson not found. Rakefile should have installed it..."
     end
-		terraform_plan_as_json_str = `#{ENV['PWD']}/old_terraform plan -state=discarded_state_not_required_for_unit_tests \ -out=terraform_fixture.tfplan > /dev/null`
+		system("#{ENV['PWD']}/old_terraform plan -state=discarded_state_not_required_for_unit_tests \
+-out=terraform_fixture.tfplan > /dev/null")
+    terraform_plan_as_json_str = system("#{tfjson_location} ./terraform_fixture.tfplan")
     puts "Got: #{terraform_plan_as_json_str}"
     if terraform_plan_as_json_str.nil? or terraform_plan_as_json_str.empty?
       raise "Mock Terraform plan was not generated."
     end
-    $terraform_plan = JSON.parse(terraform_plan_as_json_str)
+    $terraform_plan = JSON.parse(terraform_plan_json_str)
   }
   config.after(:all) {
     [ './dummy_state', './terraform_fixture.tfplan' ].each do |file|
