c0d9f86ff21527d7c248351135fb98be897b2d04
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 22:10:19 -0500

    [spec/spec_helper.rb] cnunez | Use old_terraform for generating plans.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ end
 
 RSpec.configure do |config|
   config.before(:all) {
-		system('terraform plan -state=discarded_state_not_required_for_unit_tests \
+		system('\$PWD/old_terraform plan -state=discarded_state_not_required_for_unit_tests \
 -out=terraform_fixture.tfplan > /dev/null')
 		terraform_plan_as_json_str = system('tfjson ./terraform_fixture.tfplan')
     if terraform_plan_as_json_str == ""
