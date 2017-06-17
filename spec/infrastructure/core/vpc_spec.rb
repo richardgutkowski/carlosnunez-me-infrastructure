cea91b8369ed89bcccbd87eb55e03316800a490f
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 22:39:18 -0500

    [spec/infrastructure//core/vpc_spec.rb] cnunez | These tests are redundant

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 require 'awspec'
 
 describe "Terraform plan" do
-  it "should not have failed to run" do
-    expect($terraform_plan_stderr).to be_empty
-  end
   it "should exist" do
     expect($terraform_plan).not_to be_nil
   end
