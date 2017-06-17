a12866e0efa243adeadab22bc330da0480a22eef
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 21:52:58 -0500

    [lib/terraform_helper.rb] cnunez |

--- lib/terraform_helper.rb
+++ lib/terraform_helper.rb
@@ -198,13 +198,13 @@ def download_terraform_into_working_directory!(file_name:,uri_as_string:)
 
   Zip::File.open(file_name) do |zip_file|
     zip_file.each do |file|
-      actual_file_name = file.name.sub('.zip','')
+      actual_file_name = file_name.sub('.zip','')
       puts "Extracting #{file.name} from #{file_name} and naming it #{actual_file_name}"
       if File.exist? actual_file_name
         `rm #{actual_file_name}`
       end
       file.extract(actual_file_name)
-      File.chmod(0744, file.name)
+      File.chmod(0744, actual_file_name)
     end
   end
 end
