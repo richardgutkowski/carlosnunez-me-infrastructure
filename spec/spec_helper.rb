1786c11b6a1ce6d9694dd21177d896520f39fd2d
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 22:32:51 -0500

    [spec/spec_helper.rb] cnunez | Debug why we're not getting a JSON string back.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ RSpec.configure do |config|
 		terraform_plan_as_json_str = `#{ENV['PWD']}/old_terraform plan \
 -state=discarded_state_not_required_for_unit_tests \
 -out=terraform_fixture.tfplan > /dev/null`
+    puts "Got: #{terraform_plan_as_json_str}"
     if terraform_plan_as_json_str.nil? or terraform_plan_as_json_str.empty?
       raise "Mock Terraform plan was not generated."
     end
