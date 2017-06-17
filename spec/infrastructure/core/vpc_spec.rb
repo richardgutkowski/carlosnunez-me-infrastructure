cfff31fd20b4ca533e3becbe9472ab302023bc7b
Carlos Nunez <dev@carlosnunez.me>
2017-06-16 22:45:36 -0500

    [spec/infrastructure//core/vpc_spec.rb] cnunez | Test for CIDR block equality

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 require 'awspec'
 
 describe "VPC" do
-  
+  before(:all) do
+    @vpc_details = $terraform_plan['aws_vpc.infrastructure']
+  end
+  it "should have the right CIDR block" do
+    cidr_block_expected = "10.1.0.0/16"
+    expect(@vpc_details['cidr_block']).to be_eq cidr_block_expected
+  end
 end
