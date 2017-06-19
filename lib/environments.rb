e7afa9f5170ee3a02b1e731d9374b95d811a994e
Carlos Nunez <dev@carlosnunez.me>
2017-06-18 20:38:10 -0500

    [lib/environments.rb] cnunez | Calls to AWS S3 can take a while. Cache the first results in a global to prevent having to do this more than once. Now, is a global worse than a temp file? That is an exercise left to the reader\!

--- lib/environments.rb
+++ lib/environments.rb
@@ -1,9 +1,15 @@
 def get_supported_environments
   require 'aws-sdk'
   return nil if !ENV['AWS_S3_TERRAFORM_TFVARS_BUCKET']
-  bucket_to_query = ENV['AWS_S3_TERRAFORM_TFVARS_BUCKET'].split('/')[0]
-  s3_client = Aws::S3::Client.new
-  objects_in_infrastructure_bucket = s3_client.list_objects(bucket: bucket_to_query)
+  if not $cached_s3_objects_in_infra_bucket or \
+    $cached_s3_objects_in_infra_bucket.empty?
+    bucket_to_query = ENV['AWS_S3_TERRAFORM_TFVARS_BUCKET'].split('/')[0]
+    s3_client = Aws::S3::Client.new
+    objects_in_infrastructure_bucket = s3_client.list_objects(bucket: bucket_to_query)
+    $cached_s3_objects_in_infra_bucket = objects_in_infrastructure_bucket
+  else
+    objects_in_infrastructure_bucket = $cached_s3_objects_in_infra_bucket
+  end
   tfvars_found = objects_in_infrastructure_bucket.contents.select do |s3_object|
     s3_object.key.include? 'tfvars/'
   end.map(&:key)
