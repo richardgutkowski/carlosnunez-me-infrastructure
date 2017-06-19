0eefd2b317584ec25e7fe8e2005ad7d1c2b3b0e2
Carlos Nunez <dev@carlosnunez.me>
2017-06-18 20:40:01 -0500

    [lib/environments.rb] cnunez | Is this still running multiple times

--- lib/environments.rb
+++ lib/environments.rb
@@ -2,6 +2,7 @@ def get_supported_environments
   require 'aws-sdk'
   return nil if !ENV['AWS_S3_TERRAFORM_TFVARS_BUCKET']
   if not $cached_s3_objects_in_infra_bucket
+    puts "DEBUG: Objects in TFVAR bucket haven't been cached yet. Doing this now."
     bucket_to_query = ENV['AWS_S3_TERRAFORM_TFVARS_BUCKET'].split('/')[0]
     s3_client = Aws::S3::Client.new
     objects_in_infrastructure_bucket = s3_client.list_objects(bucket: bucket_to_query)
