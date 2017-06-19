7f96a3bbd82a8157bde405664b129090d5e032a7
Carlos Nunez <dev@carlosnunez.me>
2017-06-18 21:18:47 -0500

    [vpc.us-east-1.tf] cnunez | Never mind. https://github.com/hashicorp/terraform/issues/3081

--- spec/infrastructure/core/vpc_spec.rb
+++ spec/infrastructure/core/vpc_spec.rb
@@ -8,10 +8,4 @@ describe "VPC" do
     cidr_block_expected = $terraform_tfvars['aws_vpc_cidr_block']
     expect(@vpc_details['cidr_block']).to eq(cidr_block_expected)
   end
-
-  $terraform_tfvars['aws_regions'].each do |region|
-    it "should have a VPC in region #{region}" do
-      expect(@vpc_details['provider.region']).to eq region
-    end
-  end
 end
