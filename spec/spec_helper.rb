51dd8d34f2ae34d19e7674cc6d6539e668375282
Carlos Nunez <dev@carlosnunez.me>
2017-06-18 20:22:31 -0500

    [spec/spec_helper.rb] cnunez | Show a pseudo helpful message when generating a terraofrm plan.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,8 +11,10 @@ RSpec.configure do |config|
     if not File.exist? tfjson_location
       raise "tfjson not found. Rakefile should have installed it..."
     end
+    print "INFO: Generating terraform plan now. This might take a few minutes...".yellow
 		system("#{ENV['PWD']}/old_terraform plan -state=discarded_state_not_required_for_unit_tests \
 -out=terraform_fixture.tfplan > /dev/null")
+    print "done!".green
     terraform_plan_json_serialized = `#{tfjson_location} ./terraform_fixture.tfplan`
     if terraform_plan_json_serialized.nil? or terraform_plan_json_serialized.empty?
       raise "Mock Terraform plan was not generated."
