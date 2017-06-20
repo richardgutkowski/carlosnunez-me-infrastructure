86e6e7d74747daa85dac21eaf98866873db85192
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:50:07 -0400

    [modules/kubernetes-cluster/main.tf] cnunez | debugging invalid or unknown key error

--- modules/kubernetes-cluster/main.tf
+++ modules/kubernetes-cluster/main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "kubernetes_controller" {
-  aws_region = "us-east-1"
   ami = "${data.aws_ami.kubernetes_instances.id}"
   instance_type = "${var.kubernetes_controller_instance_size}"
 }
