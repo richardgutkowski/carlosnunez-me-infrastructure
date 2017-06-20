3fd5ab7f3a1bfa61b54e4838bcd341f28737c71d
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:44:55 -0400

    [variables.tf] cnunez | variable.tf at root level?

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  description = "The AWS region onto which this infrastructure will be deployed."
+}
+
+variable "kubernetes_controller_instance_size" {
+  description = "The size for our Kubernetes controller."
+  default = "t2.micro"
+}
