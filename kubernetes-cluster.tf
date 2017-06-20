24aa721ab5e3d1ea065723c3b877da147a6fdad9
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:42:25 -0400

    [kubernetes-cluster.tf] cnunez | Forgot to set variable value in module resource

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -1,5 +1,5 @@
 module "kubernetes-cluster" {
   source = "./modules/kubernetes-cluster"
   aws_region = "${var.aws_region}"
-  kubernetes_controller_instance_size = "t2.micro"
+  kubernetes_controller_instance_size = "${var.kubernetes_controller_instance_size}"
 }
