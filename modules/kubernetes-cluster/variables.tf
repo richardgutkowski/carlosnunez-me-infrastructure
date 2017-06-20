dc508971403b651a7fa628be9d23950f735d4b6e
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:32:58 -0400

    [modules/kubernetes-cluster/variables.tf] cnunez | Forgot to refactor name here

--- modules/kubernetes-cluster/variables.tf
+++ modules/kubernetes-cluster/variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   description = "The region onto which this cluster will be deployed."
 }
 
-variable "controller_instance_size" {
+variable "kubernetes_controller_instance_size" {
   description = "The size of your Kubernetes controller."
   default = {
     us-east-1 = "t2.micro"
