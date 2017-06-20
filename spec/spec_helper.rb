f60a938e4c1fbec9adb204c3bd1295355e1ffdb3
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 22:55:57 -0400

    [spec/spec_helper.rb] cnunez | Do that file expansion thing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec'
 require 'colorize'
 Dir.glob('spec/lib/{initialisation,cleanup}/*.rb').each do |file|
   absolute_filepath = 
-    File.expand_path(File.dirname(__FILE__)) + "../#{file}"
+    File.expand_path(File.dirname(__FILE__)) + "/../#{file}"
   require_relative absolute_filepath
 end
 
