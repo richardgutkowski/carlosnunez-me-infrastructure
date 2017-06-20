c45920a154a3db346b94ed65c45fb025e9998f2d
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:04:41 -0400

    [spec/lib/initialisation/coreos.rb] cnunez | Remove globals

--- spec/lib/initialisation/coreos.rb
+++ spec/lib/initialisation/coreos.rb
@@ -7,14 +7,14 @@ def obtain_latest_coreos_version_and_ami!
     Net::HTTP.get(URI.parse(coreos_ami_release_feed_uri))
   coreos_ami_releases_json = JSON.parse(coreos_ami_releases_raw)
 
-  $latest_coreos_ami_details = {}
-  $latest_coreos_ami_details[:latest_version] = 
+  ami_data_to_return = {}
+  ami_data_to_return[:latest_version] = 
     coreos_ami_releases_json['release_info']['version']
 
   # this is a tad confusing without understanding that .delete deletes the key
   # provided from the hash in place.
   _ = coreos_ami_releases_json.delete 'release_info'
-  $latest_coreos_ami_details.merge! coreos_ami_release_json
+  ami_data_to_return.merge! coreos_ami_release_json
 
-  return $latest_coreos_ami_details
+  return ami_data_to_return
 end
