57c4aad44c8f4eec84f258bde2e3dfeff9834aeb
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 22:47:45 -0400

    [spec/lib/initialisation/coreos.rb] cnunez | Conveniently, CoreOS maintains a JSON feed of their latest AMIs. Let's use that.

---
+++ spec/lib/initialisation/coreos.rb
@@ -0,0 +1,18 @@
+require 'json'
+require 'net/http'
+
+def obtain_latest_coreos_version_and_ami!
+  coreos_ami_release_feed_uri = "https://coreos.com/dist/aws/aws-stable.json"
+  coreos_ami_releases_raw = 
+    Net::HTTP.get(URI.parse(coreos_ami_release_feed_uri))
+  coreos_ami_releases_json = JSON.parse(coreos_ami_releases_raw)
+
+  metadata_to_return = {}
+  metadata_to_return[:latest_version] = 
+    coreos_ami_releases_json['release_info']['version']
+
+  # this is a tad confusing without understanding that .delete deletes the key
+  # provided from the hash in place.
+  _ = coreos_ami_releases_json.delete 'release_info'
+  metadata_to_return.merge! coreos_ami_release_json
+end
