444e1d780c03f1d5a4ec6e277f1c2ae998289be4
Carlos Nunez <dev@carlosnunez.me>
2017-06-20 00:32:06 -0400

    [spec/infrastructure/core/kubernetes_cluster_spec.rb] cnunez | Better readability

--- spec/infrastructure/core/kubernetes_cluster_spec.rb
+++ spec/infrastructure/core/kubernetes_cluster_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe "KubernetesCluster" do
   before(:all) do
     @vpc_details = $terraform_plan['aws_vpc.infrastructure']
-    @ec2_details = $terraform_plan['aws_ec2.infrastructure.kubernetes_cluster']
-    @kube_controller_details =
-      $terraform_plan['module.kubernetes-cluster.aws_instance.kubernetes_controller']
+    @controller_details =
+      $terraform_plan['kubernetes-cluster']['aws_instance.kubernetes_controller']
   end
 
   context "Metadata" do
@@ -24,10 +23,12 @@ describe "KubernetesCluster" do
   end
 
   context "Controller" do
-    it "should be fetching the latest stable release of CoreoS" do
+    it "should be fetching the latest stable release of CoreOS for region \
+#{ENV['AWS_REGION']}" do
       latest_hvm_coreos_ami_for_this_region =
         $coreos_amis[ENV['AWS_REGION']]['hvm']
-      expect(@ami_details['image_id']).to eq latest_hvm_coreos_ami_for_this_region
+      expect(@controller_details['ami']).to \
+        eq latest_hvm_coreos_ami_for_this_region
     end
   end
 end
