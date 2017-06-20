6c722b4d67c59f2437d1e25a592e1653beca6625
Carlos Nunez <dev@carlosnunez.me>
2017-06-20 00:33:34 -0400

    [spec/infrastructure/core/kubernetes_cluster_spec.rb] cnunez | Put all contexts into the Controller context.

--- spec/infrastructure/core/kubernetes_cluster_spec.rb
+++ spec/infrastructure/core/kubernetes_cluster_spec.rb
@@ -7,22 +7,22 @@ describe "KubernetesCluster" do
       $terraform_plan['kubernetes-cluster']['aws_instance.kubernetes_controller']
   end
 
-  context "Metadata" do
-    it "should have retrieved EC2 details" do
-      expect(@ec2_details).not_to be_nil
-    end
-  end
-
-  context "VPC" do
-    it "should have a dependency on the 'infrastructure' VPC" do
-      expect(@ec2_details['depends_on']).to contain 'aws_vpc.infrastructure'
-    end
-    it "should use the subnet ID of the 'infrastructure' VPC" do
-      expect(@ec2_details['subnet_id']).to eq @vpc_details['subnet_id']
-    end
-  end
-
   context "Controller" do
+    context "Metadata" do
+      it "should have retrieved EC2 details" do
+        expect(@controller_details).not_to be_nil
+      end
+    end
+
+    context "VPC" do
+      it "should have a dependency on the 'infrastructure' VPC" do
+        expect(@controller_details['depends_on']).to contain 'aws_vpc.infrastructure'
+      end
+      it "should use the subnet ID of the 'infrastructure' VPC" do
+        expect(@controller_details['subnet_id']).to eq @vpc_details['subnet_id']
+      end
+    end
+
     it "should be fetching the latest stable release of CoreOS for region \
 #{ENV['AWS_REGION']}" do
       latest_hvm_coreos_ami_for_this_region =
