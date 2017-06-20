d61d1e997cc089ae67498db233899f8e294eeb15
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:55:43 -0400

    [kubernetes-cluster.tf] cnunez | debug why its NOW telling me that aws_region is missing when it is not

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -1,5 +1,5 @@
 module "kubernetes-cluster" {
-  aws_region = "${var.aws_region}"
+  aws_region = "${var.aws_target_region}"
   source = "./modules/kubernetes-cluster"
   kubernetes_controller_instance_size = "${var.kubernetes_controller_instance_size}"
 }
