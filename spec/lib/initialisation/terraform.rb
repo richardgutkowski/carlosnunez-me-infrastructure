0b6202b7ece29c891394bef3bb85360c3338201c
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:05:53 -0400

    [spec/lib/initialisation/terraform.rb] cnunez | Remove globals and do a terraform get to ensure that we have our modules ready.

--- spec/lib/initialisation/terraform.rb
+++ spec/lib/initialisation/terraform.rb
@@ -5,6 +5,7 @@ def initialise_global_terraform_plan!
   end
 
   print "INFO: Generating terraform plan now. This might take a few minutes...".yellow
+  system("#{ENV['PWD']}/terraform get")
   system("#{ENV['PWD']}/old_terraform plan -state=discarded_state_not_required_for_unit_tests \
 -out=terraform_fixture.tfplan > /dev/null")
   print "done!\n".green
@@ -13,15 +14,15 @@ def initialise_global_terraform_plan!
   if terraform_plan_json_serialized.nil? or terraform_plan_json_serialized.empty?
     raise "Mock Terraform plan was not generated."
   end
-  $terraform_plan = JSON.parse(terraform_plan_json_serialized)
+  terraform_plan = JSON.parse(terraform_plan_json_serialized)
 end
 
 def initialise_global_terraform_tfvars!
-  $terraform_tfvars = {}
+  terraform_tfvars = {}
   File.open("#{ENV['PWD']}/terraform.tfvars",'r').map do |line|
     tfvar_key, tfvar_value = line.split('=').map do |item|
       item.sub('"','').gsub("\n",'').gsub(' ','').gsub('"','')
     end
-    $terraform_tfvars[tfvar_key] = tfvar_value
+    terraform_tfvars[tfvar_key] = tfvar_value
   end
 end
