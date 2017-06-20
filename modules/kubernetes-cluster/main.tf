ffe806cddf8cf1bc86b8f33ab1ddbe841c7cc105
Carlos Nunez <dev@carlosnunez.me>
2017-06-19 23:21:43 -0400

    [modules/kubernetes-cluster/main.tf] cnunez | Use the updated resource name

--- modules/kubernetes-cluster/main.tf
+++ modules/kubernetes-cluster/main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "kubernetes_controller" {
-  ami = "${data.aws_ami.kubernetes_controller.id}"
+  ami = "${data.aws_ami.kubernetes_instances.id}"
   instance_type = "${var.controller_instance_size}"
 }
