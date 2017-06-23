9e5bbcbebb616f4a6d93c4bbdac34c7ad80e8ab8
Carlos Nunez <dev@carlosnunez.me>
2017-06-22 22:00:44 -0500

    [spec/infrastructure/core/kubernetes_cluster_spec.rb] cnunez | type, not size.

--- spec/infrastructure/core/kubernetes_cluster_spec.rb
+++ spec/infrastructure/core/kubernetes_cluster_spec.rb
@@ -57,7 +57,7 @@ describe "KubernetesCluster" do
       @controllers_found.keys.each do |kube_controller_resource_name|
         this_controller_details =
           @controllers_found[kube_controller_resource_name]
-        actual_instance_size = @this_controller_details['instance_size']
+        actual_instance_size = @this_controller_details['instance_type']
         expected_instance_size =
           $terraform_tfvars['kubernetes_controller_instance_size']
         expect(expected_instance_size).to eq actual_instance_size
