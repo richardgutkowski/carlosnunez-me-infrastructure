cbd24e0d8067ebf7ee9485d1a2a7662fee51e7fd
Carlos Nunez <dev@carlosnunez.me>
2017-06-22 20:03:45 -0500

    [kubernetes-cluster.tf] cnunez | Add kubernetes controller count to list of variables to be passed in.

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -2,4 +2,5 @@ module "kubernetes-cluster" {
   source = "./modules/kubernetes-cluster"
   aws_region = "${var.aws_region}"
   kubernetes_controller_instance_size = "${var.kubernetes_controller_instance_size}"
+  kubernetes_controller_count = "${var.kubernetes_controller_count}"
 }
