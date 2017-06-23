6ed5bffd4d79d0c7941e9721c181f4f324c6deb6
Carlos Nunez <dev@carlosnunez.me>
2017-06-22 22:08:48 -0500

    [spec/infrastructure/core/kubernetes_cluster_spec.rb] cnunez | this, not @this

--- spec/infrastructure/core/kubernetes_cluster_spec.rb
+++ spec/infrastructure/core/kubernetes_cluster_spec.rb
@@ -57,8 +57,7 @@ describe "KubernetesCluster" do
       @controllers_found.keys.each do |kube_controller_resource_name|
         this_controller_details =
           @controllers_found[kube_controller_resource_name]
-        puts "got: #{this_controller_details}"
-        actual_instance_size = @this_controller_details['instance_type']
+        actual_instance_size = this_controller_details['instance_type']
         expected_instance_size =
           $terraform_tfvars['kubernetes_controller_instance_size']
         expect(expected_instance_size).to eq actual_instance_size
