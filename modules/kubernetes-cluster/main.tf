c67dd71a9dbe86db7e488b705ce0ac3ce8d651ef
Carlos Nunez <dev@carlosnunez.me>
2017-06-22 20:09:06 -0500

    cnunez | add support for count to resource

--- modules/kubernetes-cluster/main.tf
+++ modules/kubernetes-cluster/main.tf
@@ -5,4 +5,5 @@ provider "aws" {
 resource "aws_instance" "kubernetes_controller" {
   ami = "${data.aws_ami.kubernetes_instances.id}"
   instance_type = "${var.kubernetes_controller_instance_size}"
+  count = "${var.kubernetes_controller_count}"
 }
