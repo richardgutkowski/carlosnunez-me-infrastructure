3efd117a1b8154b2c9b1f8c8103661cfaea95784
Carlos Nunez <dev@carlosnunez.me>
2017-06-25 18:49:02 -0500

    [modules/aws-ec2-key/main.tf] cnunez | Actually, the key_name variable should be generic.

--- ec2_key.tf
+++ ec2_key.tf
@@ -1,5 +1,5 @@
 module "ec2-instance-key" {
   source = "./modules/aws-ec2-key"
-  key_name = "${var.ec2_key_name}"
+  key_name = "${var.environment}"
   key_pair = "${var.ec2_public_key}"
 }
--- modules/aws-ec2-key/main.tf
+++ modules/aws-ec2-key/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "keypair" {
-  key_name = "${var.environment}"
+  key_name = "${var.ec2_key_name}"
   public_key = "${var.public_key}"
 }
