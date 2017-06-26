36cc1cfaf77abb816fe066b68392e6d4db3ababe
Carlos Nunez <dev@carlosnunez.me>
2017-06-25 19:27:47 -0500

    [modules/aws-ec2-key/main.tf] cnunez | oops, wrong var naming

--- modules/aws-ec2-key/main.tf
+++ modules/aws-ec2-key/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "keypair" {
-  key_name = "${var.ec2_key_name}"
-  public_key = "${var.ec2_public_key}"
+  key_name = "${var.key_name}"
+  public_key = "${var.public_key}"
 }
