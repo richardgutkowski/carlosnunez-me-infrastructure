1a4e38293f7a7a2c5f6c8371696209f4188663cf
Carlos Nunez <dev@carlosnunez.me>
2017-07-03 17:29:06 -0500

    [lib/environment_variable_processing.rb] cnunez | handle a single message for now.

--- lib/environment_variable_processing.rb
+++ lib/environment_variable_processing.rb
@@ -1,6 +1,6 @@
 def process_env_vars!(required_env_vars:, optional_env_vars:)
   parsed_options = {
-    :error_messages => []
+    :error_message => []
   }
   required_env_vars.each do |env_var, env_var_properties|
     actual_env_var_value = ENV[env_var]
@@ -8,14 +8,14 @@ def process_env_vars!(required_env_vars:, optional_env_vars:)
       if not env_var_properties[:default_value].nil?
         actual_env_var_value = env_var_properties[:default_value]
       else
-        parsed_options[:error_messages].push
+        parsed_options[:error_message].push
           "ERROR: Required environment variable not found: #{env_var}".red
       end
     end
     supported_env_var_values = env_var_properties[:supported_values]
     if not supported_env_var_values.nil? and
       not supported_env_var_values.include? actual_env_var_value
-      parsed_options[:error_messages].push
+      parsed_options[:error_message].push
         "ERROR: \"#{actual_env_var_value}\" is not supported. \
 Supported values are: #{supported_env_var_values}".red
     end
