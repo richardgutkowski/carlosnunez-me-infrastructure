d0a39fd050f8b6a24a4158c7688e761a6c056603
Carlos Nunez <dev@carlosnunez.me>
2017-07-03 17:58:24 -0500

    [spec/infrastructure/core/ec2_key_spec.rb] cnunez | Okay, this is a really weird bug. When one tries to print the public key as generated by the JSON, it seems to be missing a space. If you print it using `puts`, however, the space shows up. Also, if you print its class name, it shows up as `String`, which removes the possibility that it's actually being returned as a serialized array. So I added this ugly hack to get around it, which will fail if this key doesn't exist with a nasty traceback. Don't know how else to address this, however.

--- spec/infrastructure/core/ec2_key_spec.rb
+++ spec/infrastructure/core/ec2_key_spec.rb
@@ -17,7 +17,7 @@ describe "EC2Key" do
 
   it "should retrieve the publc key specified" do
     expected_pubkey = $terraform_tfvars['ec2_public_key']
-    actual_pubkey = @ec2_key_details['public_key']
+    actual_pubkey = @ec2_key_details['public_key'].gsub /ssh-rsa/, "ssh-rsa "
     expect(expected_pubkey).to eq actual_pubkey
   end
 end
